e476b46f175824382745b901fbe4baf00e0ebfcc
Jeonghyeon <jeonghyeon0208@g.hongik.ac.kr>
2023-09-18 03:36:40 +0900

    설정창 비밀번호 가능

--- app/src/main/java/com/effort/project_application/SettingFragment.kt
+++ app/src/main/java/com/effort/project_application/SettingFragment.kt
@@ -1,59 +1,122 @@
 package com.effort.project_application
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SettingFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SettingFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var btnSetLock: Button
+    private lateinit var btnSetDelLock: Button
+    private lateinit var btnChangePwd: Button
+    private var lock = true
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+        val view = inflater.inflate(R.layout.fragment_setting, container, false)
+
+        // 버튼 위젯 초기화
+        btnSetLock = view.findViewById(R.id.btnSetLock)
+        btnSetDelLock = view.findViewById(R.id.btnSetDelLock)
+        btnChangePwd = view.findViewById(R.id.btnChangePwd)
+
+        // 잠금 설정 버튼 클릭 리스너 등록
+        btnSetLock.setOnClickListener {
+            val intent = Intent(requireContext(), AppPassWordActivity::class.java).apply {
+                putExtra(AppLockConst.type, AppLockConst.ENABLE_PASSLOCK)
+            }
+            startActivityForResult(intent, AppLockConst.ENABLE_PASSLOCK)
+        }
+
+        // 잠금 비활성화 버튼 클릭 리스너 등록
+        btnSetDelLock.setOnClickListener{
+            val intent = Intent(requireContext(), AppPassWordActivity::class.java).apply {
+                putExtra(AppLockConst.type, AppLockConst.DISABLE_PASSLOCK)
+            }
+            startActivityForResult(intent, AppLockConst.DISABLE_PASSLOCK)
+        }
+
+        // 암호 변경 버튼 클릭 리스너 등록
+        btnChangePwd.setOnClickListener {
+            val intent = Intent(requireContext(), AppPassWordActivity::class.java).apply {
+                putExtra(AppLockConst.type, AppLockConst.CHANGE_PASSWORD)
+            }
+            startActivityForResult(intent, AppLockConst.CHANGE_PASSWORD)
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SettingFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SettingFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when(requestCode){
+            AppLockConst.ENABLE_PASSLOCK ->
+                if(resultCode == Activity.RESULT_OK){
+                    Toast.makeText(requireContext(), "암호 설정 됨", Toast.LENGTH_SHORT).show()
+                    init()
+                    lock  = false
                 }
+
+            AppLockConst.DISABLE_PASSLOCK ->
+                if(resultCode == Activity.RESULT_OK){
+                    Toast.makeText(requireContext(), "암호 삭제 됨", Toast.LENGTH_SHORT).show()
+                    init()
+                }
+
+            AppLockConst.CHANGE_PASSWORD ->
+                if(resultCode == Activity.RESULT_OK){
+                    Toast.makeText(requireContext(), "암호 변경 됨", Toast.LENGTH_SHORT).show()
+                    lock = false
+                }
+
+            AppLockConst.UNLOCK_PASSWORD ->
+                if(resultCode == Activity.RESULT_OK){
+                    Toast.makeText(requireContext(), "잠금 해제 됨", Toast.LENGTH_SHORT).show()
+                    lock = false
+                }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if(lock && AppLock(requireContext()).isPassLockSet()){
+            val intent = Intent(requireContext(), AppPassWordActivity::class.java).apply {
+                putExtra(AppLockConst.type, AppLockConst.UNLOCK_PASSWORD)
             }
+            startActivityForResult(intent, AppLockConst.UNLOCK_PASSWORD)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (AppLock(requireContext()).isPassLockSet()) {
+            lock = true
+        }
+    }
+
+    private fun init(){
+        if (AppLock(requireContext()).isPassLockSet()){
+            btnSetLock.isEnabled = false
+            btnSetDelLock.isEnabled = true
+            btnChangePwd.isEnabled = true
+            lock = true
+        }
+        else{
+            btnSetLock.isEnabled = true
+            btnSetDelLock.isEnabled = false
+            btnChangePwd.isEnabled = false
+            lock = false
+        }
     }
 }
